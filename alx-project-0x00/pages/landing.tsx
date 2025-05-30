002c3af51c9830bb33bc617b276ce69a1480759f
Omar mohamed <omarabdou2002@yahoo.com>
2025-05-30 20:51:17 +0300

    Done 3

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,13 @@
+import Card from "@/components/Card"
+
 const Landing: React.FC =  () => {
   return (
-    <div>
+    <>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
     </div>
+    </>
   )
 }
 export default Landing
